bf838143c327b13349556216db8f4a395ab71e3d
chernandodelaf <chernandodelaf@tudelft.nl>
2025-09-15 15:44:21 +0200

    implemented a tweet search by ID

--- scrapper.py
+++ scrapper.py
@@ -2,6 +2,9 @@ import asyncio
 from twikit import Client
 from dotenv import load_dotenv
 import os
+from datetime import datetime
+
+print("Starting scrapper...\n")
 
 load_dotenv()
 
@@ -12,7 +15,6 @@ PASSWORD = os.getenv("TWITTER_PASSWORD")
 # Initialize client
 client = Client("en-US")
 
-
 async def main():
     await client.login(
         auth_info_1=USERNAME,
@@ -21,13 +23,27 @@ async def main():
         cookies_file="cookies.json",
     )
 
-    # Search for 5 tweets containing the keyword "climate"
-    tweets = await client.search_tweet("climate", "Latest", 5)
-
+    query = """global AND warming AND snow AND 50 AND states"""
+    n_tweets = 100000
+    tweets = await client.search_tweet(query, "Latest", n_tweets)
+    example_id = "954037043079688192"
+    
+    ids = []
     for tweet in tweets:
-        print(tweet.user.name, "\n", tweet.text, "\n", tweet.created_at)
+        print(f"Username: {tweet.user.name} \nDate: {tweet.created_at} \nID: {tweet.id} \n Text:{tweet.text}")
+        ids.append(tweet.id)
         print("----------------------------------------")
 
+
+    print("\nSearching the tweet of ID:", example_id)
+    if id in ids:
+        print(f"Tweet with ID {example_id} found.")
+        index = ids.index(example_id)
+        print(f"Date in Twitter format: {tweets[index].created_at}")
+        print(f"Username: {tweets[index].user.name}")
+        print(f"Text: {tweets[index].text}")
+    else:
+        print(f"Tweet with ID {example_id} not found.")
 
 asyncio.run(main())
 
